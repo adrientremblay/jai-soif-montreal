0e5663ee0775870e23f8ddcd1c7b90600e4045d8
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-01 20:03:15 -0500

    Generated a basic spring application template

---
+++ jaisoifmontreal/src/main/java/com/adrientremblay/jaisoifmontreal/JaisoifmontrealApplication.java
@@ -0,0 +1,13 @@
+package com.adrientremblay.jaisoifmontreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaisoifmontrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaisoifmontrealApplication.class, args);
+	}
+
+}
---
+++ jaisoifmontreal/src/test/java/com/adrientremblay/jaisoifmontreal/JaisoifmontrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrientremblay.jaisoifmontreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaisoifmontrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
