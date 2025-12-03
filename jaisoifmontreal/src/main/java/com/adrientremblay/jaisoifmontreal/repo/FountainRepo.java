e02ce34499ab232b2ba87b88aa4caba8fd68edb5
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-03 14:19:38 -0500

    Created FountainRepo interface

---
+++ jaisoifmontreal/src/main/java/com/adrientremblay/jaisoifmontreal/repo/FountainRepo.java
@@ -0,0 +1,8 @@
+package com.adrientremblay.jaisoifmontreal.repo;
+
+import com.adrientremblay.jaisoifmontreal.model.Fountain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FountainRepo extends JpaRepository<Fountain, Integer> {
+
+}
