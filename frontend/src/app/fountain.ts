e7e4a82190c862f44ac821f3f058e2794febb2a6
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-05 11:57:58 -0500

    Fountain interface on frontend

---
+++ frontend/src/app/fountain.ts
@@ -0,0 +1,10 @@
+export interface Fountain {
+    id: number;
+    borough: string;
+    placeName: string;
+    placeType: string;
+    intersection: string;
+    notes: string;
+    lattitude: number;
+    longitude: number;
+}
