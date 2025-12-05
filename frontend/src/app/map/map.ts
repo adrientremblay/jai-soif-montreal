22c0d661166d0ab1f1c59bf46b41a57aec14c23e
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-05 13:41:00 -0500

    Code to add fountains to the map

--- frontend/src/app/map/map.ts
+++ frontend/src/app/map/map.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID } from '@angular/core';
+import { Fountain } from '../fountain';
+import { FountainService } from '../fountain.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-map', 
@@ -12,6 +15,7 @@ export class MapComponent implements OnInit, OnDestroy {
   @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
   map: any;
   private platformId = inject(PLATFORM_ID);
+  public fountains: Fountain[] = [];
 
   async ngOnInit() {
     if (isPlatformBrowser(this.platformId)) { // SSR check to ensure this runs in the browser as GL JS requires a browser environment
@@ -21,10 +25,30 @@ export class MapComponent implements OnInit, OnDestroy {
       this.map = new mapboxgl.Map({
         accessToken: 'pk.eyJ1IjoiYWRyaWVudHJlbWJsYXkiLCJhIjoiY21pZGkzZmhqMDZydjJqcG51cTV0cDMzOCJ9.uBD-0FwBXgdibDCiSF4Hlw',
         container: this.mapContainer.nativeElement, // Reference to the map container element
-        center: [-98.54818, 40.00811], // Center coordinates for map over the continental US
-        zoom: 4, // Initial zoom level
+        center: [-73.5674, 45.5019, ], // Center coordinates for map over the continental US
+        zoom: 11, // Initial zoom level
       });
+
+      this.getFountains();
+      this.fountains.forEach(fountain => {
+        new mapboxgl.Marker()
+          .setLngLat([fountain.longitude, fountain.lattitude])
+          .addTo(this.map);
+      })
     }
+  }
+
+  constructor(private fountainService: FountainService){}
+
+  public getFountains(): void {
+    this.fountainService.getFountains().subscribe(
+      (response: Fountain[]) => {
+        this.fountains = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error.message);
+      }
+    )
   }
 
   ngOnDestroy(): void {
