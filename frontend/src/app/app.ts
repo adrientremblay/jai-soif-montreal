41c94e381afc903a03b6862e0ad7bcc44be9653e
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-05 12:18:52 -0500

    Created app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Fountain } from './fountain';
+import { FountainService } from './fountain.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+
+  public fountains: Fountain[] = [];
+
+  constructor(private fountainService: FountainService){}
+
+  ngOnInit(): void {
+    this.getFountains();
+  }
+
+  public getFountains(): void {
+    this.fountainService.getFountains().subscribe(
+      (response: Fountain[]) => {
+        this.fountains = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
