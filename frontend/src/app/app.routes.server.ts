5ead5bb6e8b912dff0082dd6d6257aef94424a4d
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-12 19:37:43 -0500

    Fix angular app build to not prerender

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,4 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
+export const serverRoutes: ServerRoute[] = [];
+// Prerendering doesn't make sense for this app
